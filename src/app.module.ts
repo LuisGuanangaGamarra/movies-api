b4e746272d008ce7a338087892f2f16dc23a6e02
Luis Guananga <luisgamarra_97@hotmail.com>
2025-10-05 14:12:43 -0500

    move jwt module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,48 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import DataSource from './shared/infra/data-source';
 import { UsersModule } from './users/users.module';
 import { JwtStrategy } from './auth/infrastructure/jwt.strategy';
 import { MoviesModule } from './movies/movies.module';
+import { resolve } from 'path';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: resolve(__dirname, `../.env`) ?? undefined,
     }),
-    TypeOrmModule.forRoot(DataSource.options),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          ...(DataSource.options as PostgresConnectionOptions),
+          host: config.get<string>('DB_HOST'),
+          port: config.get<number>('DB_PORT')!,
+          username: config.get<string>('DB_USER'),
+          password: config.get<string>('DB_PASS'),
+          database: config.get<string>('DB_NAME'),
+        };
+      },
+    }),
+    JwtModule.registerAsync({
+      global: true,
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: config.get<string>('JWT_EXPIRES'),
+          algorithm: 'HS256',
+          issuer: config.get<string>('JWT_ISSUER'),
+          audience: config.get<string>('JWT_AUDIENCE'),
+        },
+      }),
+    }),
     UsersModule,
     AuthModule,
     MoviesModule,
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,28 +1,11 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './presentation/auth.controller';
 import { JwtStrategy } from './infrastructure/jwt.strategy';
 import { UsersModule } from '../users/users.module';
 import { LoginUseCase } from './application/use-cases/login.usecase';
-import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [
-    UsersModule,
-    JwtModule.registerAsync({
-      global: true,
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        secret: config.get<string>('JWT_SECRET'),
-        signOptions: {
-          expiresIn: config.get<string>('JWT_EXPIRES'),
-          algorithm: 'HS256',
-          issuer: config.get<string>('JWT_ISSUER'),
-          audience: config.get<string>('JWT_AUDIENCE'),
-        },
-      }),
-    }),
-  ],
+  imports: [UsersModule],
   controllers: [AuthController],
   providers: [JwtStrategy, LoginUseCase],
 })
